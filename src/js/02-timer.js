723b4746b1360aab2b2c744b8538c73572f6c11e
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-06-26 16:12:30 +0300

    task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,25 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+const currentDate = new Date();
+flatpickr("#datetime-picker", {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: currentDate,
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+});
+const dateInput = document.querySelector("#datetime-picker")._flatpickr;
+
+const startBtn = document.querySelector('button[data-start]');
+startBtn.setAttribute("disabled", "disabled"); 
+
+
+// if (dateInput > currentDate) {
+//     startBtn.removeAttribute("disabled"); 
+
+// }
+//Notiflix.Notify.failure('Please, choose a date in the future');
