9eb8e1a23e32d4742a888920271d5d90bea6b2ac
M-Tyshchenko <m.tish2406@gmail.com>
2023-06-27 23:56:19 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,20 @@
+import Notiflix from 'notiflix';
+Notiflix.Notify.init({
+ useIcon: false,
+})
+
+const formEl = document.querySelector('.form');
+let formDataObj = {};
+
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('submitted')
+})
+
+formEl.addEventListener('change', onFormInput);
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +23,18 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+function onFormInput(event) {
+  formDataObj[event.target.name] = event.target.value;
+  console.log(formDataObj);
+
+  
+}
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
