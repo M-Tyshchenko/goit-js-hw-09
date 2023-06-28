786cf05a652a3340dbe11406759ea2b98f59d98d
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-06-28 13:49:56 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,40 +1,64 @@
 import Notiflix from 'notiflix';
 Notiflix.Notify.init({
- useIcon: false,
-})
+  useIcon: false,
+});
 
 const formEl = document.querySelector('.form');
-let formDataObj = {};
+let firstDelay = null;
+let delayStep = null;
+let amount = null;
 
-formEl.addEventListener('submit', (event) => {
-  event.preventDefault();
-  console.log('submitted')
-})
+formData();
 
-formEl.addEventListener('change', onFormInput);
+// події при натисканні кнопки сабміту-------------------------
+formEl.addEventListener('submit', onSubmitBtnClick);
+
+function onSubmitBtnClick(event) {
+  event.preventDefault(); // щоб при сабміті сторінка не перезавантажувалась
+  
+  setTimeout(() => {
+    let delay = firstDelay;
+    for (let position = 1; position <= amount; position += 1) {
+      createPromise(position, delay);
+      delay += delayStep;
+    }
+  }, firstDelay);
+}
+
+//-------------------------------------------------------------
+
+// відслідковую введені дані в поля форми----------------------
+formEl.addEventListener('change', formData);
+
+function formData() {
+  firstDelay = Number(formEl.elements.delay.value);
+  
+  delayStep = Number(formEl.elements.step.value);
+  
+  amount = Number(formEl.elements.amount.value);
+}
+//-------------------------------------------------------------
 
 
-
+// функція створення промісу-----------------------------------
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        // Reject
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+
+  promise.then(value => {
+  Notiflix.Notify.success(value);
+}).catch(error => {
+  Notiflix.Notify.failure(error);
+});
 }
-
-function onFormInput(event) {
-  formDataObj[event.target.name] = event.target.value;
-  console.log(formDataObj);
-
-  
-}
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+//-------------------------------------------------------------
